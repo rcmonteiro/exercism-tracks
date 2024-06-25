d50a2d28076515bed3cd52ccea499265c1651b0a
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-06-25 15:31:14 -0300

    chore: typescript collatz conjecture

--- typescript/collatz-conjecture/collatz-conjecture.ts
+++ typescript/collatz-conjecture/collatz-conjecture.ts
@@ -1,3 +1,10 @@
-export function steps(count: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export const steps = (count: number, stepsCount: number = 0): number => {
+  if (count < 1 || !Number.isInteger(count)) {
+    throw new Error('Only positive integers are allowed')
+  }
+  if (count === 1) {
+    return stepsCount;
+  }
+  count = count % 2 === 0 ? count / 2 : count * 3 + 1;
+  return steps(count, (stepsCount + 1))
 }
