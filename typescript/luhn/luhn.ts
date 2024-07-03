4056f8e77407e4d05c59c7c9243c8a4dad70c527
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-07-03 09:35:32 -0300

    chore: one more ts challenge

--- typescript/luhn/luhn.ts
+++ typescript/luhn/luhn.ts
@@ -1,3 +1,13 @@
-export function valid(digitString: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function valid(digitString: string): boolean {
+  if (!/^[0-9\s]+$/g.test(digitString)) return false 
+  const digits = digitString.replace(/\D/g, '')
+  if (digits.length <= 1) return false
+  
+  return [...digits.replace(/\D/g, '')]
+    .reverse()
+    .map((digit, index) => {
+      return (index + 1) % 2 === 0 ? (+digit * 2) > 9 ? +digit * 2 - 9 : +digit * 2 : +digit
+    })
+    .reverse()
+    .reduce((sum, digit) => sum + digit, 0) % 10 === 0
 }
